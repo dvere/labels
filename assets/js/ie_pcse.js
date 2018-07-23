c78105db8580184be357e2073b97716a4e380fd8
Anthony La Porte <ant@ukbsd.org>
2018-07-23 17:43:10 +0100

    just what context are we in??

--- assets/js/ie_pcse.js
+++ assets/js/ie_pcse.js
@@ -88,9 +88,9 @@ function doLabels(items) {
     w.document.open();
     w.document.write("<pre>" + fmt + tt + '}$</pre>');
     w.document.close();
+    w.close();
     var r = document.querySelector('[name=oframe]').contentWindow.document.execCommand('print', false, null);
     if (!r) w.print()
-    w.close();
 }
 
 $.when($.ready).then(function() {
