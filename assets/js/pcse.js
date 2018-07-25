e4c7129d14617baae717b088ff9c560253256c56
Anthony La Porte <ant@ukbsd.org>
2018-07-25 10:52:44 +0100

    Redo last due to breakeges

--- assets/js/pcse.js
+++ assets/js/pcse.js
@@ -1,4 +1,5 @@
 function getTrackingId(orderId,deliveryDate,items) {
+
     var pcseData = { tab: 'ReadyForDespatch', SearchOn: 5 };
     pcseData.DateRangeStart = pcseData.DateRangeEnd = deliveryDate;
 
@@ -17,13 +18,32 @@ function getTrackingId(orderId,deliveryDate,items) {
         return $(text).find('tr:contains('+ orderId +')').children().eq(9);
     }).then(function(td) {
         items.unshift($(td).html().split('<br>').sort().reverse()[0]);
-		doLabels(items);
+        doLabels(items);
     });
 }
 
 function doLabels(items) {
-    var fmt='${^XA^DFR:DELIVERY.GRF^PON^LS7^LH0,10^FO5,5^GB780,1200,4^FS^FO5,240^GB780,860,3^FS^FO5,420^GB480,180,3^FS^FO482,240^GB302,360,3^FS^FO5,660^GB780,1,3^FS^FO5,820^GB780,1,3^FS^FO5,970^GB780,1,3^FS^FO215,60^BY2^BCN,150,N^FN0^FS^FO5,25^A0N,30^FB780,1,0,C^FN0^FS^FO15,275^A0N,140^FB480,1,0,C^FN1^FS^FO15,455^A0N,140^FB480,1,0,C^FN2^FS^FO500,365^A0N,140^FB280,1,0,C^FN3^FS^FO5,615^A0N,40^FB780,1,0,C^FN4^FS^FO15,700^A0N,100^FB780,1,0,C^FN6^FS^FO150,840^A0N,24^FN4^FS^FO150,870^A0N,24^FN5^FS^FO150,900^A0N,24^FN6^FS^FO15,1000^A0N,100^FB780,1,0,C^FN8^FS^FO170,1120^BY2^BCN,55,Y^FN7^FS^XZ\n';
-
+    var fmt = '${^XA^DFR:DELIVERY.GRF^PON^LS0^LH25,15\n\
+    ^FO5,5^GB780,1200,4^FS\n\
+    ^FO5,240^GB780,860,3^FS\n\
+    ^FO5,420^GB480,180,3^FS\n\
+    ^FO482,240^GB302,360,3^FS\n\
+    ^FO5,660^GB780,1,3^FS\n\
+    ^FO5,820^GB780,1,3^FS\n\
+    ^FO5,970^GB780,1,3^FS\n\
+    ^FO215,60^BY2^BCN,150,N^FN0^FS\n\
+    ^FO5,25^A0N,30^FB780,1,0,C^FN0^FS\n\
+    ^FO15,275^A0N,140^FB480,1,0,C^FN1^FS\n\
+    ^FO15,455^A0N,140^FB480,1,0,C^FN2^FS\n\
+    ^FO500,365^A0N,140^FB280,1,0,C^FN3^FS\n\
+    ^FO5,615^A0N,40^FB780,1,0,C^FN4^FS\n\
+    ^FO15,700^A0N,100^FB780,1,0,C^FN6^FS\n\
+    ^FO150,840^A0N,24^FN4^FS\n\
+    ^FO150,870^A0N,24^FN5^FS\n\
+    ^FO150,900^A0N,24^FN6^FS\n\
+    ^FO15,1000^A0N,100^FB780,1,0,C^FN8^FS\n\
+    ^FO170,1120^BY2^BCN,55,Y^FN7^FS\n^XZ\n\
+';
     var ls = "^XA\n^XFR:DELIVERY.GRF^FS\n";
     var le = "\n^XZ";
     var qty = items.pop();
@@ -61,6 +81,7 @@ function doLabels(items) {
 
     $('#output').remove();
     $('body').removeAttr('style');
+
 }
 
 $.when($.ready).then(function() {
@@ -71,10 +92,16 @@ $.when($.ready).then(function() {
     if (qty == null || qty == "" || isNaN(qty)) {
         console.log("Input quantity error or cancelled by user");
         return;
-       };
-    var serviceCentres={BASINGSTOKE:'BS',BECKTON:'CV',BIRMINGHAM:'BP',BRISTOL:'BL',CAMBRIDGE:'CB',GATWICK:'CW',LEEDS:'LD',LETCHWORTH:'LE',MANCHESTER:'MA',MEDWAY:'ME',MILTON:'MK',NEWCASTLE:'NE',NORWICH:'NR',NOTTINGHAM:'NG',PLYMOUTH:'PL',READING:'NB',SLOUGH:'3S',SOUTHAMPTON:'SO',SWINDON:'SW',TELFORD:'TF',WARWICK:'MC'};
-
-	var orderId = $('h3').eq(0).text().trim().split(' ')[2];
+    };
+    var serviceCentres = {
+        BASINGSTOKE: 'BS', BECKTON: 'CV', BIRMINGHAM: 'BP', BRISTOL: 'BL',
+        CAMBRIDGE: 'CB', GATWICK: 'CW', LEEDS: 'LD', LETCHWORTH: 'LE',
+        MANCHESTER: 'MA', MEDWAY: 'ME', MILTON: 'MK', NEWCASTLE: 'NE',
+        NORWICH: 'NR', NOTTINGHAM: 'NG', PLYMOUTH: 'PL', READING: 'NB',
+        SLOUGH: '3S', SOUTHAMPTON: 'SO', SWINDON: 'SW', TELFORD: 'TF',
+        WARWICK: 'MC'
+    };
+    var orderId = $('h3').eq(0).text().trim().split(' ')[2];
     var svc = $('nav.account-links').find('li').eq(1).text().trim();
     var svcCode = serviceCentres[svc.substr(0,svc.indexOf(' ')).toUpperCase()];
     var orderDetail = {};
@@ -90,6 +117,7 @@ $.when($.ready).then(function() {
     var address = orderDetail['Shipping Address'].split(',');
     var address_1 = address.shift();
     var postcode = address.pop();
+
     var items = [
         orderDetail['Delivery Route And Stop'].substr(0,4),
         orderDetail['Location Code'],
@@ -99,7 +127,6 @@ $.when($.ready).then(function() {
         postcode,
         orderId,
         qty
-        ];
-
+    ];
     getTrackingId(orderId, deliveryDate, items);
 });
