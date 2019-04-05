e30f38abc42067abb833f496d5b3c9633f20974b
dvere <dvere@users.noreply.github.com>
2019-04-05 14:41:53 +0100

    Update logistics.js

--- assets/js/logistics.js
+++ assets/js/logistics.js
@@ -1,25 +1,28 @@
-function getIt() {
-  var url = '/consignments/';
-	var sDate = $('#qD').val();
-	var data = {
-	  q: 'collected:SW',
-	  count: 5000,
-		client_id: 11270,
-		received_at: sDate,
-	};
-  $.ajax({
-    url: url,
-    data: data,
-    success: success
-  });
+function getJSON(cDate) {
+	var url = '/consignments/',
+			data = {
+				q: 'collected:SW',
+	  		count: 5000,
+				client_id: 11270,
+				received_at: cDate
+			};
+	var jqxhr = $.get(url, data, function(json) {
+		return json;		
+	})
+		.fail(function(){
+			alert('Request failed');
+		});
 };
 
 function embedDiv(){
- $('<div id="embededDiv"></div>').insertBefore('#nav-search');
+	$('<div>').addClass('ed').insertBefore('#nav-search')
+	  .append($('<input>').addClass('cDate').attr('type', 'date'));
+		// onchange?
 };
 
 function popUpModal() {
 };
 
 $.when($.ready).then(function() {
+	var div = 
 };
