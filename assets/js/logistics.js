8ccc166dd3eda167dc9e195439351a5f598325bb
dvere <dvere@users.noreply.github.com>
2019-04-14 20:04:37 +0100

    Update logistics.js

--- assets/js/logistics.js
+++ assets/js/logistics.js
@@ -17,33 +17,11 @@ var url = '/consignments/',
 
 function showEvents(t){
   var u = '/consignments/' + t + '/events',
-      cEvents = $('<div>', {
-        'id': 'cEvents'
-      })
-      .css({
-        margin: '140px auto',
-        width: 580,
-        height: 400,
-        padding: 40,
-        backgroundColor: 'white',
-        zIndex: '999',
-        overflowY: 'scroll'
-      }),
-      cHeader = $('<div>',{class: 'events-header'})
-        .css({
-          display: 'grid',
-          gridTemplateColumns: '140px 40px 180px 140px',
-          color: 'rgba(255,255,255,1)',
-          backgroundColor: 'rgba(55,55,55,1)'
-        });
+      cEvents = $('<div>', {'id': 'cEvents'}),
+      cHeader = $('<div>',{class: 'events-header'});
 
   $.each(['Timestamp', 'SC','Event','User'], function(i, t){
-    $('<div>',{
-      class: 'events-header-item',
-      text: t
-    })
-      .css({padding: 4})
-      .appendTo(cHeader);
+    $('<div>',{class: 'events-header-item', text: t}).appendTo(cHeader);
   });
   
   $('#cAudits').empty();
@@ -52,11 +30,7 @@ function showEvents(t){
   
   $.getJSON(u,function(json){
     $.each(json, function(i, obj) {
-      var cEvent = $('<div>',{class: 'event'})
-        .css({
-          display: 'grid',
-          gridTemplateColumns: '140px 40px 180px 140px'
-        });
+      var cEvent = $('<div>',{class: 'event'});
       obj.service_centre = obj.service_centre || {code: 'NA'};
       obj.user = obj.user || {username: 'NA'};
       $('<div>', {'class': 'event-item', 'text': obj.timestamp}).appendTo(cEvent);
@@ -65,8 +39,6 @@ function showEvents(t){
       $('<div>', {'class': 'event-item', 'text': obj.user.username}).appendTo(cEvent);
       cEvent.appendTo(cEvents);
     });
-    $('.event').css({border: '1px solid rgba(0,0,0,.7)', borderTopStyle: 'none', backgroundColor: 'white'});
-    $('.event-item').css({padding: 4});
     $('#cAudits').fadeIn();
   })
     .fail(function(){
@@ -75,21 +47,10 @@ function showEvents(t){
 }
 
 function getCollectedCons() {
-  var cHeader = $('<div>',{class: 'consignments-header'})
-    .css({
-      display: 'grid',
-      gridTemplateColumns: '120px 80px 80px 80px 140px',
-      color: 'rgba(255,255,255,1)',
-      backgroundColor: 'rgba(55,55,55,1)'
-    });
+  var cHeader = $('<div>',{class: 'consignments-header'});
 
   $.each(['Traking No','Type','Route','Location','Status'], function(i, t){
-    $('<div>',{
-      class: 'consignments-header-item',
-      text: t
-    })
-      .css({padding: 4})
-      .appendTo(cHeader);
+    $('<div>',{class: 'consignments-header-item', text: t}).appendTo(cHeader);
   });
   
   $('#cConsignments').empty();
@@ -100,12 +61,7 @@ function getCollectedCons() {
   $.getJSON(url, data, function(json){
     $.each(json, function(i, obj) {
       if(obj.status != 'DELIVERED') {
-        var cConsignment = $('<div>', {class: 'consignment'})
-          .css({
-            display: 'grid',
-            gridTemplateColumns: '120px 80px 80px 80px 140px'
-        });
-
+        var cConsignment = $('<div>', {class: 'consignment'});
         $('<div>', {'class': 'consignment-item', 'text': obj.tracking_number, 'onclick': 'showEvents(' + obj.id + ')', 'id': obj.id}).appendTo(cConsignment);
         $('<div>', {'class': 'consignment-item', 'text': obj.package_type}).appendTo(cConsignment);
         $('<div>', {'class': 'consignment-item', 'text': obj.requested_route}).appendTo(cConsignment);
@@ -114,7 +70,6 @@ function getCollectedCons() {
         $('#cConsignments').append(cConsignment);
       };
     });
-    $('.consignment-item').css({padding: 4});
   })
     .fail(function(){
       console.log('Consignments Request Failed');
@@ -122,19 +77,19 @@ function getCollectedCons() {
 }
 
 function addPartsToDOM(){
+  $('<link>', {rel: 'stylesheet;', href: 'https://dvere.github.io/labels/assets/css/logitics.css?v='+ $.now()})
+    .appendTo($('head'));
   $('#cInsert').remove();
   var cInsert = $('<div>',{'id':'cInsert'})
     .append($('<input>', {'id':'cDate', 'type':'date'}))
     .append($('<button>', {'id': 'cButton', 'text': 'Lookup Collections', 'onclick': 'getCollectedCons()'}))
-    .append($('<div>', {'id': 'cConsignments'}).css({width: 580, margin: '0 auto'}))
+    .append($('<div>', {'id': 'cConsignments'}))
     .append($('<div>', {'id':'cAudits'}));
   $('#breadcrumbs').after(cInsert);
-	$('#cDate').css({lineHeight: '1.2em', marginLeft: 20});
-	$('#cButton').css({lineHeight: '1.3em',marginLeft: 4});
+
 	$('#cAudits').click(function(){
     $(this).fadeOut();
-  })
-  .css({display: 'none', position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,.75)', zIndex: '999'});
+  });
 };
 
 $.when($.ready).then(function() {
