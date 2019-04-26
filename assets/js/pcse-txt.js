8dbdc16d36b1bc5c880e51000e823290846becb1
Anthony La Porte <alaporte@citysprint.co.uk>
2019-04-26 13:45:28 +0100

    Another formatting tweak

--- assets/js/pcse-txt.js
+++ assets/js/pcse-txt.js
@@ -59,11 +59,9 @@ function doLabels(items) {
 ^FO150,900^A0N,24^FN6^FS
 ^FO15,1000^A0N,100^FB780,1,0,C^FN8^FS
 ^FO170,1120^BY2^BCN,55,Y^FN7^FS
-^XZ
-
-`,
-  labelStart = '^XA^XFR:DELIVERY.GRF\n',
-  labelEnd = '^XZ\n\n',
+^XZ`,
+  labelStart = '\n\n^XA^XFR:DELIVERY.GRF\n',
+  labelEnd = '^XZ',
   pieces = items.pop(),
   fileName = items[7],
   commonFields = items.map(function(value, index) {
