23cf7b1f48d80fd44abefb691de5b12f35800379
Anthony La Porte <ant@ukbsd.org>
2019-04-29 23:36:36 +0100

    first try at brining it all together

---
+++ assets/js/pcse-labels.js
@@ -0,0 +1,201 @@
+function buildLabels(order) {
+  let pcseData = {tab: order.tab},
+    edDate = order.edDate,
+    items = order.items;
+  
+  if (edDate != 'TBA') { 
+    edDate = edDate.split('/').reverse().join('-');
+    pcseData.DateRangeStart = pcseData.DateRangeEnd = edDate;
+    pcseData.SearchOn = 5; 
+  }
+
+  // this would smell better with plain fetch...
+  let pcseInit = {
+    url: '/portal/Logistics/Orders',
+    xhrFields: {
+      withCredentials: true
+    },
+    method: 'GET',
+    dataType: 'html',
+    data: pcseData
+  }
+
+  $.ajax(pcseInit).done(function(data) {
+    return $(data).text();
+  }).then(function(text) {
+    return $(text).find('tr:contains('+ order.id +')').children().eq(9);
+  }).then(function(td) {
+    items.unshift($(td).html().split('<br>').sort().reverse()[0]);
+    let filename = items[1] + '_' + items[7],
+        zpl = makeZpl(items);
+    actionLabels(zpl, filename);
+  });
+}
+
+function makeZpl(items) {
+  var labelFormat = `
+^FX ${new Date().toISOString()}
+^XA
+^DFR:DELIVERY.GRF
+^FO5,5^GB780,1200,4^FS
+^FO5,240^GB780,860,3^FS
+^FO5,420^GB480,180,3^FS
+^FO482,240^GB302,360,3^FS
+^FO5,660^GB780,1,3^FS
+^FO5,820^GB780,1,3^FS
+^FO5,970^GB780,1,3^FS
+^FO215,60^BY2^BCN,150,N^FN0^FS
+^FO5,25^A0N,30^FB780,1,0,C^FN0^FS
+^FO15,275^A0N,140^FB480,1,0,C^FN1^FS
+^FO15,455^A0N,140^FB480,1,0,C^FN2^FS
+^FO500,365^A0N,140^FB280,1,0,C^FN3^FS
+^FO5,615^A0N,40^FB780,1,0,C^FN4^FS
+^FO15,700^A0N,100^FB780,1,0,C^FN6^FS
+^FO150,840^A0N,24^FN4^FS
+^FO150,870^A0N,24^FN5^FS
+^FO150,900^A0N,24^FN6^FS
+^FO15,1000^A0N,100^FB780,1,0,C^FN8^FS
+^FO170,1120^BY2^BCN,55,Y^FN7^FS
+^XZ`,
+  labelStart = '\n\n^XA^XFR:DELIVERY.GRF\n',
+  labelEnd = '^XZ',
+  pieces = items.pop(),
+  commonFields = items.map(function(value, index) {
+    return '^FN' + index + '^FD' + value + '^FS';
+  }).join('\n'),
+  pkgLabels = '';
+    
+  for (var i = pieces; i > 0; i--) {
+    let label = labelStart + commonFields +
+      '\n^FN8^FDPieces: ' + i + ' of ' + pieces +
+      '^FS\n' + labelEnd;
+    pkgLabels += label;
+  }
+  var zpl = labelFormat + pkgLabels;
+  return zpl;
+}
+
+function actionLabels(data, filename){
+  var x = $('#foo').attr('data-file-type'),
+   fileType = Object.is(x, undefined) ? 'prn' : x;
+  switch (fileType){
+    case 'pdf':
+      pdfFromZpl(data, filename);
+      break;
+    case 'txt':
+      downloadAsFile(data, filename, 'text/plain');
+      break;
+    case 'prn':
+      directPrint(data);
+      break;
+  } 
+}
+
+function directPrint(zpl){
+  var printWindow = window.open();
+  printWindow.document.open('text/plain')
+  printWindow.document.write(zpl);
+  printWindow.document.close();
+  printWindow.focus();
+  printWindow.print();
+  printWindow.close();
+}
+
+function downloadAsFile(data, filename, type) {
+  var a = document.createElement('a'),
+    blob = new Blob([data], { type: type }),
+    url = window.URL.createObjectURL(blob);
+  a.style = 'display: none';
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(url);
+  a.remove();
+};
+
+function pdfFromZpl(zpl, filename){
+  var fd = new FormData(),
+      headers = new Headers(),
+      url = 'https://lab1.dvere.org/l/';
+
+  fd.append('file',zpl);
+  headers.accept = 'application/pdf';
+
+  var init = {
+    method: 'POST',
+    headers: headers,
+    mode: 'cors',
+    body: fd,
+    credentials: 'omit'
+  },
+  myRequest = new Request(url, init);
+
+  fetch(myRequest).then(function(response){
+    return response.blob();
+  }).then(function(blob){
+    downloadAsFile(blob, filename, 'application/pdf');
+  });
+}
+
+$.when($.ready).then(function() {
+  // bail if not in an order
+  if (window.location.pathname.split('/')[3] != 'Order') return;
+  var status = $('#OrderStatusId').val(),
+    tabs = {
+      100001: 'Complete',
+      141560002: 'ReadyForDespatch',
+      141560003: 'Despatched'
+    };
+  // bail on invalid status 
+  if (!(status in tabs)) {
+    return;
+  }
+  
+  var qty = prompt("Enter number of packages:", 1);
+  // bail on invalid quantity
+  if (qty == null || qty == "" || isNaN(qty)) {
+    console.log("Input quantity error or cancelled by user");
+    return;
+  };
+
+  var serviceCentres = {
+    BASINGSTOKE: 'BS', BECKTON: 'CV', BIRMINGHAM: 'BP', BRISTOL: 'BL',
+    CAMBRIDGE: 'CB', GATWICK: 'CW', LEEDS: 'LD', LETCHWORTH: 'LE',
+    MANCHESTER: 'MA', MEDWAY: 'ME', MILTON: 'MK', NEWCASTLE: 'NE',
+    NORWICH: 'NR', NOTTINGHAM: 'NG', PLYMOUTH: 'PL', READING: 'NB',
+    SLOUGH: '3S', SOUTHAMPTON: 'SO', SWINDON: 'SW', TELFORD: 'TF',
+    WARWICK: 'MC'
+  };
+
+  var orderId = $('h3').eq(0).text().trim().split(' ')[2],
+      svc = $('nav.account-links').find('li').eq(1).text().trim(),
+      svcCode = serviceCentres[svc.substr(0,svc.indexOf(' ')).toUpperCase()],
+      orderDetail = {};
+
+  $('.pcss-order-summary').find('.col-md-5').each(function( i, e ) {
+    var v =  $('.pcss-order-summary').find('.col-md-7').eq(i).text().trim();
+    orderDetail[$(e).text()] = v;
+  });
+
+  var address = orderDetail['Shipping Address'].split(','),
+      address_1 = address.shift(),
+      postcode = address.pop();
+  
+  var order = {
+    id: orderId,
+    tab: tabs[status],
+    edDate: orderDetail['Expected Delivery Date'],
+    items: [
+      orderDetail['Delivery Route And Stop'].substr(0,4),
+      orderDetail['Location Code'],
+      svcCode,
+      address_1,
+      address,
+      postcode,
+      orderId,
+      qty
+    ]
+  };
+  buildLabels(order);
+});
